c8c64cfda5a7eab8687d6f19358da91edfb1d5fe
Heath Kehoe <yaheath@gmail.com>
2023-01-25 22:42:45 -0600

    finish up

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::vec::Vec;
-use regex::Regex;
 extern crate advent_lib;
 use advent_lib::read::read_grouped_input;
 
@@ -9,8 +8,7 @@ type RuleNum = usize;
 
 #[derive(Clone, Debug)]
 enum Rule {
-    AltRule(Vec<RuleNum>, Vec<RuleNum>),
-    Seq(Vec<RuleNum>),
+    AltSeq(Vec<Vec<RuleNum>>),
     Char(char),
 }
 
@@ -27,17 +25,13 @@ impl FromStr for RuleEntry {
         let id = spl.next().unwrap().parse::<usize>().unwrap();
         let rule = spl.next().unwrap();
         if rule.contains("\"") {
-            return Ok(RuleEntry { id, rule: Rule::Char(rule.chars().skip(1).next().unwrap()) });
+            Ok(RuleEntry { id, rule: Rule::Char(rule.chars().skip(1).next().unwrap()) })
         }
-        let mut seqs: Vec<Vec<RuleNum>> = rule.split(" | ").map(
-            |sp| sp.split(' ').map(|n| n.parse::<usize>().unwrap()).collect()
-        ).collect();
-        match seqs.len() {
-            1 => Ok(RuleEntry { id, rule: Rule::Seq(seqs.pop().unwrap()) }),
-            2 => Ok(RuleEntry { id, rule: Rule::AltRule(
-                        seqs.swap_remove(0), seqs.pop().unwrap()
-                    ) }),
-            _ => Err(())
+        else {
+            let seqs: Vec<Vec<RuleNum>> = rule.split(" | ").map(
+                |sp| sp.split(' ').map(|n| n.parse::<usize>().unwrap()).collect()
+            ).collect();
+            Ok(RuleEntry { id, rule: Rule::AltSeq(seqs) })
         }
     }
 }
@@ -50,27 +44,59 @@ fn parse_rules(input: &Vec<String>) -> HashMap<RuleNum, Rule> {
         .collect()
 }
 
-fn mkregex_id(id: RuleNum, rules: &HashMap<RuleNum, Rule>) -> String {
-    mkregex(&rules[&id], rules)
+fn process_seq<'a>(s: &'a str, seq: &Vec<RuleNum>, rules: &HashMap<RuleNum, Rule>) -> Vec<&'a str> {
+    seq.iter()
+        .try_fold(vec![s], |solutions, rule_num| {
+            let next_solutions: Vec<&str> = solutions
+                .iter()
+                .flat_map(|input| process_rules(input, *rule_num, rules))
+                .collect();
+            if next_solutions.is_empty() {
+                None
+            } else {
+                Some(next_solutions)
+            }
+        })
+        .unwrap_or_else(Vec::new)
 }
-fn mkregex(rule: &Rule, rules: &HashMap<RuleNum, Rule>) -> String {
+
+fn process_rules<'a>(s: &'a str, rule_num: RuleNum, rules: &HashMap<RuleNum, Rule>) -> Vec<&'a str> {
+    let rule = &rules[&rule_num];
     match rule {
-        Rule::Char(c) => String::from_iter([c]),
-        Rule::AltRule(a, b) =>
-            format!("(?:(?:{})|(?:{}))", mkregex(&Rule::Seq(a.clone()), rules), mkregex(&Rule::Seq(b.clone()), rules)),
-        Rule::Seq(v) =>
-            v.iter().map(|n| mkregex_id(*n, rules)).collect::<Vec<_>>().join(""),
+        Rule::Char(c) => s.chars()
+            .next()
+            .filter(|f| *f == *c)
+            .map(|_| &s[1..])
+            .into_iter()
+            .collect(),
+
+        Rule::AltSeq(v) => v.iter()
+            .flat_map(|seq| process_seq(s, seq, rules))
+            .collect(),
     }
 }
 
+fn is_match(s: &str, rules: &HashMap<RuleNum, Rule>) -> bool {
+    process_rules(s, 0, rules)
+        .iter()
+        .find(|&&r| r == "")
+        .is_some()
+}
+
 fn part1(rules: &HashMap<RuleNum, Rule>, strings: &Vec<String>) -> usize {
-    let reg = format!("^{}$", mkregex_id(0, rules));
-    let regex = Regex::new(&reg).unwrap();
-    strings.iter().filter(|s| regex.is_match(s)).count()
+    strings.iter().filter(|s| is_match(s, rules)).count()
+}
+
+fn part2(rules: &HashMap<RuleNum, Rule>, strings: &Vec<String>) -> usize {
+    let mut rules = rules.clone();
+    rules.insert(8, Rule::AltSeq(vec![vec![42], vec![42, 8]]));
+    rules.insert(11, Rule::AltSeq(vec![vec![42, 31], vec![42, 11, 31]]));
+    strings.iter().filter(|s| is_match(s, &rules)).count()
 }
 
 fn main() {
     let input: Vec<Vec<String>> = read_grouped_input::<String>();
     let rules = parse_rules(&input[0]);
     println!("Part 1: {}", part1(&rules, &input[1]));
+    println!("Part 2: {}", part2(&rules, &input[1]));
 }
